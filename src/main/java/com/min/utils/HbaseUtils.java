fd9b07f39c50af2c19b02df348b0c3236f7cae21
PC-20171018UAPS\Administrator <654279133@qq.com>
2017-11-24 11:29:13 +0800

    修改

--- src/main/java/com/min/utils/HbaseUtils.java
+++ src/main/java/com/min/utils/HbaseUtils.java
@@ -16,19 +16,20 @@ public class HbaseUtils {
 	 * 转换rowkey,将传入的身份证和平台ID处理为rowkey
 	 */
 	public static String transformRowkey(String idcard, String siteid, String mobile) {
-		long keyTmp = 0;
+		//long keyTmp = 0;
 		try {
 			// 去掉身份证最后一位
-			String str = idcard.substring(0, idcard.length() - 1);
+			// String str = idcard.substring(0, idcard.length() - 1);
 			// 反转
-			String newStr = new StringBuilder(str).reverse().toString();
+			String newStr = new StringBuilder(mobile).reverse().toString();
 			// 身份证号加上平台id
-			keyTmp = Long.valueOf(newStr) + Long.valueOf(siteid) + Long.valueOf(mobile);
+			// keyTmp = Long.valueOf(newStr) + Long.valueOf(siteid) + Long.valueOf(mobile);
+			return newStr + idcard + siteid;
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			return null;
 		}
-		return idcard.substring(idcard.length() - 1) + String.valueOf(keyTmp);
+
 	}
 
 	/*
@@ -36,7 +37,7 @@ public class HbaseUtils {
 	 * 如果不需要返回下划线，类属性名不要采用驼峰命名法 举例：addTime 返回 add_time addtime 返回 addtime
 	 */
 	public static String switchParam(String name) {
-		//System.out.println("hjbghj:"+name);
+		// System.out.println("hjbghj:"+name);
 		if (name.matches("[a-z]+[A-Z][a-z]+([A-Z][a-z]+)*")) {
 			Pattern pattern = Pattern.compile("[A-Z]");
 			Matcher matcher = pattern.matcher(name);
